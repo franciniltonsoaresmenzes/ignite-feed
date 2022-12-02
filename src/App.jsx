6ab7d75d33adc6d86aa4c31416143f16fb98bd22
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-12-02 19:27:19 -0300

    setup inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Post } from "./Post"
+
+import "./styles.css"
+
+export function App() {
+
+  return (
+    <div>
+      <Post 
+        author="Diego Fernandes" 
+        content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum ."
+      />
+    </div>
+  )
+}
