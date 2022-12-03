f1d3682345960ef633580e9f671c3aeb8d9a3ed0
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-12-03 20:06:25 -0300

    setup inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
 
-import "./styles.css"
+import styles from "./App.module.css"
+import "./global.css"
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
 
   return (
     <div>
-      <Post 
-        author="Diego Fernandes" 
-        content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum ."
-      />
+      <Header/>
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, veritatis facere aliquid, iure, alias ipsa assumenda eos et adipisci odio deserunt veniam dignissimos reiciendis velit eaque porro! Modi, velit neque.</main>
+      </div>
     </div>
   )
 }
