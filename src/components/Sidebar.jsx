6f0df248383c2d35acb85f9084d7cff257f6a750
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-12-05 18:17:28 -0300

    Sidebar concluido

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from "phosphor-react"
 import styles from "./Sidebar.module.css"
 
 export function Sidebar() {
@@ -5,7 +6,7 @@ export function Sidebar() {
     <aside className={styles.sidebar} >
       <img 
         className={styles.cover}
-        src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40" 
+        src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40"
       />
 
       <div className={styles.profile} >
@@ -18,6 +19,7 @@ export function Sidebar() {
       </div>
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
